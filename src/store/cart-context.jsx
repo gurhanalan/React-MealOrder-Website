5e677aa48bb9b7948d377d10045db22aa81c33e3
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-11-23 13:42:48 +0300

    context autofill

--- src/store/cart-context.jsx
+++ src/store/cart-context.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const CartContext = React.createContext({
     items: [],
-    totalAmaount: 0,
+    totalAmount: 0,
     addItem: (item) => {},
-    removeItem: (item) => {},
+    removeItem: (id) => {},
 });
 
 export default CartContext;
