51776446f24955022ca8f40f3edee804cd60a393
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-11-26 14:39:32 +0300

    cart remove items reducer function

--- src/store/CartProvider.jsx
+++ src/store/CartProvider.jsx
@@ -31,6 +31,29 @@ const cartReducer = (state, action) => {
             items: updatedItems,
             totalAmount: updatedTotalAmount,
         };
+    } else if (action.type === "REMOVE") {
+        const existingCartItemIndex = state.items.findIndex(
+            (item) => item.id === action.id
+        );
+        const existingCartItem = state.items[existingCartItemIndex];
+        const updatedTotalAmount = state.totalAmount - existingCartItem.price;
+        let updatedItems;
+
+        if (existingCartItem.amount === 1) {
+            updatedItems = state.items.filter((item) => item.id !== action.id);
+        } else {
+            const updatedItem = {
+                ...existingCartItem,
+                amount: existingCartItem.amount - 1,
+            };
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = updatedItem;
+        }
+
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount,
+        };
     }
     return defaultCartState;
 };
@@ -43,7 +66,9 @@ const CartProvider = ({ children }) => {
     const addItemtoCartHandler = (item) => {
         dispatchCartAction({ type: "ADD", item: item });
     };
-    const removeItemFromCartHandler = (id) => {};
+    const removeItemFromCartHandler = (id) => {
+        dispatchCartAction({ type: "REMOVE", id: id });
+    };
     const cartContext = {
         items: cartState.items,
         totalAmount: cartState.totalAmount,
